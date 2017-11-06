29f2c13e2b7ea9eb0e5b25388e0d5e647cbd6a6b
Esteban Mino <efmino@uc.cl>
2017-11-05 22:25:43 -0300

    naive requests

--- api_server/controllers/cart.js
+++ api_server/controllers/cart.js
@@ -1,10 +1,14 @@
 const config = require('../config');
+const Transactions = require('./transactions');
 
 exports.processCart = function(req, res, next) {
-  console.log(req.body);
+  var responses = {};
   for(i in req.body) {
-    console.log(req.body[i]);
-  }
-  return res.send({body: req.body[1]});
 
+    console.log("se" + req.body[i].product_id);
+    responses[req.body[i].product_id] = Transactions.buyProduct(req.body[i].product_id,req.body[i].amount, req.user._id);
+  }
+
+  console.log("response" + responses["1"]);
+  return res.send({transactions: responses});
 }
--- api_server/controllers/transactions.js
+++ api_server/controllers/transactions.js
@@ -1,16 +1,19 @@
 const Transaction = require('../models/transaction');
 const config = require('../config');
+var request=require('request');
+var async_request = require('async');
 
 const MAX_PURCHASES_PER_DAY = 3;
+const APPLICATION_TOKEN = "6d876925-a71d-4379-93aa-6144138dc8fc";
+const GROUP = "G2";
+const TRANSACTIONS_URL = 'http://arqss16.ing.puc.cl/transactions/';
 
-exports.buyProduct = function(req, res, next) {
+
+exports.buyProduct = function(productId, amount, userId) {
   const yesterday = new Date(Date.now() - 864e5);
-  const userId = req.body.user_id;
-  const productId = req.body.product;
-  const amount = req.body.amount;
-  const id = req.user.id;
+  //const userId = req.body.user_id;
 
-  console.log(id);
+  console.log(userId);
 
   Transaction.count({ userId, productId })
     .where('createdAt')
@@ -22,8 +25,31 @@ exports.buyProduct = function(req, res, next) {
       if (count + amount > MAX_PURCHASES_PER_DAY) {
         return res.status(401).send({ error: 'Exceeds daily purchase limit'})
       } else {
-        // TODO Executes purchase
-        return res.status(200).send({ succes: 'Bought'})
+
+        var jsonRequest = {
+          "application_token": APPLICATION_TOKEN,
+          "product": productId,
+          "id": GROUP,
+          "amount": amount,
+          "user_id": userId
+        };
+        var optionsRequest = {
+             url: TRANSACTIONS_URL,
+             method: 'POST',
+             headers: {
+               'Content-Type': 'application/json'
+             },
+             json: jsonRequest
+        };
+       request(optionsRequest, function(err, resp, body) {
+         if (resp && (resp.statusCode === 200 || resp.statusCode === 201)) {
+           console.log(resp.statusCode + resp.body.status + resp.body.transaction_status_code);
+           return "Success"
+         } else {
+           console.log(resp.statusCode + body.status + body.transaction_status_code);
+           return "Error"
+         }
+       });
 
       }
     });
